8beea9c0eb07f4fd17ab6e593b1556fa6be12747
Lemon <s.lemon.2016@mymail.jcsu.edu>
2018-02-06 12:41:03 -0500

    first commit

---
+++ src/Books.java
@@ -0,0 +1,20 @@
+
+public class Books {
+String BookAuthor;
+int BookYearPublish;
+double BookISBNnumber;
+int BookPrice;
+	public Books(String Author, int Year, double ISBN, int Price) {
+		// TODO Auto-generated constructor stub
+
+BookAuthor=Author;
+BookYearPublish=Year;
+BookISBNnumber=ISBN;
+BookPrice=Price;
+	
+	
+	
+	
+	
+}
+}
---
+++ src/BooksP.java
@@ -0,0 +1,24 @@
+
+public class BooksP {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	Books B=new Books("Stephen King",1986,783.45,9);	
+	Books C=new Books("J.K Rowling",2000,978.04,10);
+		
+	System.out.println(B.BookAuthor+ B.BookYearPublish+ B.BookISBNnumber+ B.BookPrice);	
+	
+	System.out.println(C.BookAuthor+ C.BookYearPublish+ C.BookISBNnumber+ C.BookPrice);	
+		
+	}
+
+}
+
+	
+	
+	
+	
+
